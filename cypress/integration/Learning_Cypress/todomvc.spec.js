7bf6eac4ed2660f037889d967cc1042b37e54267
Fahadailmon47 <fouad.ibex@gmail.com>
2021-09-09 17:23:46 +0500

    new change

--- cypress/integration/Learning_Cypress/todomvc.spec.js
+++ cypress/integration/Learning_Cypress/todomvc.spec.js
@@ -1,1 +1,10 @@
 /// <reference types="cypress" />
+
+
+it('Should be able to add new Todo to the list', () => {
+    cy.visit('http://todomvc-app-for-testing.surge.sh')
+    cy.get('.new-todo', {timeout: 6000}).type("Clean Room{enter}")
+    cy.get('.toggle').click()
+    cy.contains('Clear completed').click()
+
+})
