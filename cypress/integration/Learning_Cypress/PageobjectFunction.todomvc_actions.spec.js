0aeffa804cc9ca58e304f926108fea19730a170d
Fahadailmon47 <fouad.ibex@gmail.com>
2021-09-13 15:46:19 +0500

    Changes

---
+++ cypress/integration/Learning_Cypress/PageobjectFunction.todomvc_actions.spec.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+import {
+  navigate,
+  addTodo,
+  validateTodoText,
+  toggleTodo,
+  clearCompleted,
+  validateTodoCompletedState,
+  validateToggleState,
+  validateNumberOfTodosShown,
+} from '..\cypress\page-objects\todo-page-function.js'
+
+
+describe('todo actions', () => {
+
+  beforeEach(() => {
+    navigate()
+
+    addTodo('Clean room')
+  })
+
+  it('should add a new todo to the list', () => {
+    validateTodoText(0, 'Clean room')
+
+    validateToggleState(0, false)
+  })
+
+  describe('toggling todos', () => {
+    it('should toggle test correctly', () => {
+      toggleTodo(0)
+      validateTodoCompletedState(0, true)
+    })
+
+    it('should clear completed', () => {
+      toggleTodo(0)
+
+      clearCompleted()
+
+      validateNumberOfTodosShown(0)
+    })
+  })
+})
